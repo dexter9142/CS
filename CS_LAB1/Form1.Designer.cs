ae41e3e9b49cc061d4c21a5c7cc605b98d607913
dexter9142 <56232253+dexter9142@users.noreply.github.com>
2021-09-17 22:50:54 +0300

    Add files via upload
    Lab 1

---
+++ CS_LAB1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CS_LAB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(48, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Convert .audit to .json";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 226);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Convertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CS_LAB1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CS_LAB1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = @"E:\Audit Policiy\To Work\CIS_DC_SERVER_2012_Level_1_v2.2.0.audit";
+            string filePath1= @"E:\Audit Policiy\To Work\CIS_DC_SERVER_2012_Level_2_v2.2.0.audit";
+            string filePath2 = @"E:\Audit Policiy\To Work\CIS_DC_SERVER_2012_R2_Level_1_v2.4.0.audit";
+
+            string destinationPath = @"E:\Audit Policiy\To Work\Replaced\Audit_1.json";
+            string destinationPath1 = @"E:\Audit Policiy\To Work\Replaced\Audit_2.json";
+            string destinationPath2 = @"E:\Audit Policiy\To Work\Replaced\Audit_3.json";
+
+
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList();
+
+
+
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+        ///////////////////////////////////////////////////////////////////// 
+            try
+            {
+                if (File.Exists(destinationPath))
+                {
+                    File.Delete(destinationPath);
+                }
+
+                using (StreamWriter sw = File.CreateText(destinationPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            ///////////////////////////////////////////////////////////
+            try
+            {
+                List<string> lines1 = new List<string>();
+                lines1 = File.ReadAllLines(filePath1).ToList();
+                if (File.Exists(destinationPath1))
+                {
+                    File.Delete(destinationPath1);
+                }
+
+                using (StreamWriter sw = File.CreateText(destinationPath1))
+                {
+                    foreach (string line in lines1)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            //////////////////////////////////////////////////////////////
+            try
+            {
+                List<string> lines2 = new List<string>();
+                lines2 = File.ReadAllLines(filePath2).ToList();
+
+                if (File.Exists(destinationPath2))
+                {
+                    File.Delete(destinationPath2);
+                }
+
+                using (StreamWriter sw = File.CreateText(destinationPath2))
+                {
+                    foreach (string line in lines2)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            MessageBox.Show("Convertion done.\n Path: E:\\Audit Policiy\\To Work\\Replaced", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Console.ReadLine();
+            
+        }
+
+     
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
